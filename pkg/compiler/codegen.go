b56189ba834f4894be36bad007004c3d8ea6104b
Evgenii Stratonikov <evgeniy@nspcc.ru>
2020-08-21 11:41:10 +0300

    compiler: allow init statement in if

--- pkg/compiler/codegen.go
+++ pkg/compiler/codegen.go
@@ -560,6 +560,9 @@ func (c *codegen) Visit(node ast.Node) ast.Visitor {
 		lElse := c.newLabel()
 		lElseEnd := c.newLabel()
 
+		if n.Init != nil {
+			ast.Walk(c, n.Init)
+		}
 		if n.Cond != nil {
 			c.emitBoolExpr(n.Cond, true, false, lElse)
 		}
